fa46bad94ac407a0d6b83936a899ed0c2ea79376
mehonoshinat <dustown@mail.ru>
2019-05-17 11:35:00 +0500

    MyFirstTest

---
+++ src/test/java/ru/org/autotest/FirstTest.java
@@ -0,0 +1,31 @@
+package ru.org.autotest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstTest {
+    private static WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+
+    }
+
+    @Test
+    public void userLogin() {
+
+    }
+
+    @After
+    public void tearDown(){
+
+   }
+}
